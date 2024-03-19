dfe638e084a50cae5cecc8db774ef6eadf38d32d
DeonteElysia <163978952+DeonteElysia@users.noreply.github.com>
2024-03-19 15:59:48 +0800

    update

--- deploy-vless.sh
+++
@@ -1,9 +0,0 @@
-last_commit=$(git log -1 --format=%cd --date=unix)
-
-current_time=$(date +%s)
-
-time_difference=$(( (current_time - last_commit) / 3600 ))
-
-if [ $time_difference -lt 1 ]; then
-    npx wrangler pages deploy --project-name vless ./
-fi
